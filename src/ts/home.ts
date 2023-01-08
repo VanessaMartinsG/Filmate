2cd7b931220350a2c0107d1baa2093c823b955a8
brunopittaf <caiobrunopittaf@gmail.com>
2023-01-08 09:49:29 -0300

    feat: floating menu implementado

--- src/ts/home.ts
+++ src/ts/home.ts
@@ -1,1 +1,34 @@
 import '../scss/home.scss';
+
+import searchImage from '../img/search.svg';
+import searchOrangeImage from '../img/search-orange.svg';
+
+
+const floatingMenu = document.querySelector(".floating-menu");
+
+const clickFloatingMenu = () => {
+  const homeIcon = floatingMenu?.querySelector(".home");
+  const searchIcon = floatingMenu?.querySelector<HTMLImageElement>(".search img");
+  const personIcon = floatingMenu?.querySelector(".person");
+
+  homeIcon?.addEventListener("click", () => {
+    homeIcon.classList.add("active");
+    searchIcon!.src = searchImage;
+    personIcon?.classList.remove("active");
+  })
+
+  searchIcon?.addEventListener("click", () => {
+    searchIcon!.src = searchOrangeImage;
+    homeIcon?.classList.remove("active");
+    personIcon?.classList.remove("active");
+  })
+
+  personIcon?.addEventListener("click", () => {
+    personIcon.classList.add("active");
+    searchIcon!.src = searchImage;
+    homeIcon?.classList.remove("active");
+  })
+
+}
+
+floatingMenu ? clickFloatingMenu() : '';
